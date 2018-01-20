3245bd90bf6f2bda3980c005e16f505cc217dce3
Pamela Kwong <pkwong1@sva.edu>
2018-01-19 21:21:01 -0500

    created two ellipses and rectangles

--- work/pamelakwong/week02/01_shape/sketch.js
+++ work/pamelakwong/week02/01_shape/sketch.js
@@ -2,13 +2,27 @@ function setup() {
   createCanvas(500,500);
   background(200);
 
-  fill(255,0,0);
-  ellipseMode(CORNER);
-  ellipse(250,250,100,100);
+  // fill(255,0,0);
+  // ellipseMode(CORNER);
+  // ellipse(250,250,100,100);
+  //
+  //
+  // fill(0,255,0);
+  // rectMode(CENTER);
+  // rect(250,250,100,100);
 
+  //explore the other parameters in Mode
 
-  fill(0,255,0);
-  rectMode(CENTER);
-  rect(250,250,100,100);
+}
+
+function draw() {
+  noStroke();
+  fill(mouseX * 0.7);
+//  ellipse(250, 250, mouseX, mouseY);
+
+  ellipse(mouseY, mouseX, 50, 50);
+  ellipse(mouseY, mouseY, 80, 80);
+//  rect(mouseX, mouseY, 90, 90);
+
 
 }
