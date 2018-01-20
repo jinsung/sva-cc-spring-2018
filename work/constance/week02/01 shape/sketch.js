3d59b7f1162e8092abd8ce4bc5354c1dbda781c9
constanceread <constanceread@live.co.uk>
2018-01-19 21:01:56 -0500

    shape

---
+++ work/constance/week02/01 shape/sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+  createCanvas(500,500);
+  background(200);
+
+  fill(220,50,60);
+  ellipseMode(CORNER);
+  ellipse(250,250,100,100);
+
+
+fill(20,130,90);
+rectMode(CENTER);
+rect(250,250,100,100);
+
+}
