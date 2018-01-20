7817378aa321352eaf1f6ced885bbdef1334dfe5
yumitogo <yumiyumi0123@gmail.com>
2018-01-19 21:00:23 -0500

    Shape example

---
+++ work/yumi/week_02/01_Shape/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(500, 500);
+  background(230);
+
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(250, 250, 100, 100);
+
+  fill(0, 255, 0);
+  rect(250, 250, 100, 100);
+
+}
+
+function draw(){
+
+
+}
