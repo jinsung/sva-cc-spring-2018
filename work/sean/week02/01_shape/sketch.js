fe7597a13a7799408dfc4cdf7daa6e78e0f49a2f
Sean Kirkpatrick <seungkim94@gmail.com>
2018-01-19 21:23:17 -0500

    This is week 02

--- work/sean/week02/01_shape/sketch.js
+++ work/sean/week02/01_shape/sketch.js
@@ -1,16 +1,10 @@
 function setup() {
   createCanvas(500,500);
   background(200);
-
-  fill(255, 0, 0);
-  ellipseMode(CORNER);
-  ellipse(250, 250, 100, 100);
-
-  fill(0, 255, 0);
-  rectMode(CENTER);
-  rect(250, 250, 100, 100);
 }
 
 function draw () {
-
+  noStroke();
+  fill(mouseX * 0.5)
+  ellipse (250, 250, mouseX, mouseY);
 }
