e0f3cdf4f3790eb96db66f9190cacacbcc3ab722
constanceread <constanceread@live.co.uk>
2018-01-19 21:12:02 -0500

    circle thing

--- work/constance/week02/01 shape/sketch.js
+++ work/constance/week02/01 shape/sketch.js
@@ -1,14 +1,13 @@
 function setup(){
   createCanvas(500,500);
-  background(200);
+background(200,30,150);
 
-  fill(220,50,60);
-  ellipseMode(CORNER);
-  ellipse(250,250,100,100);
+}
 
+function draw(){
 
-fill(20,130,90);
-rectMode(CENTER);
-rect(250,250,100,100);
+noStroke();
+fill(mouseX*0.5);
+ellipse(250,250,mouseX,mouseY);
 
 }
