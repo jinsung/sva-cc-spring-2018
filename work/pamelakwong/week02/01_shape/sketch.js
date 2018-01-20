3ab723d0dc9bda978cab4204cba68d31891b21cd
Pamela Kwong <pkwong1@sva.edu>
2018-01-19 21:00:32 -0500

    created circle and rectangle

---
+++ work/pamelakwong/week02/01_shape/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500,500);
+  background(200);
+
+  fill(255,0,0);
+  ellipseMode(CORNER);
+  ellipse(250,250,100,100);
+
+
+  fill(0,255,0);
+  rectMode(CENTER);
+  rect(250,250,100,100);
+
+}
