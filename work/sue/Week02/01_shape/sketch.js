1163b077fb7fa076818f8d219ca7e5f1a2a2750e
Your Name <slee170@sva.edu>
2018-01-19 21:20:39 -0500

    This is Week 02

--- work/sue/Week02/01_shape/sketch.js
+++ work/sue/Week02/01_shape/sketch.js
@@ -1,12 +1,12 @@
 function setup() {
   createCanvas(500,500);
-  background(200);
+  background(200)
 
-  fill(255, 0,0);
-  ellipseMode(CORNER)
-  ellipse(250, 250, 100, 100);
+}
 
-  fill(0, 255, 0);
-  rectMode(CENTER)
-  rect(250, 250, 100, 100)
+function draw() {
+  noStroke();
+
+  fill(mouseX,0,0);
+  ellipse(mouseX, 250, mouseX, mouseY)
 }
