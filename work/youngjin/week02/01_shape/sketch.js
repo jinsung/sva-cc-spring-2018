8ce120fb6e9d895206093e4d4bf42f45e6d1d610
Youngjin <ylim7@sva.com>
2018-01-19 21:01:56 -0500

    shape...

--- work/youngjin/week02/01_shape/sketch.js
+++ work/youngjin/week02/01_shape/sketch.js
@@ -1,12 +1,7 @@
 function setup() {
 createCanvas(500, 500);
-background(200);
+}
 
-fill(255, 0, 0);
-ellipseMode(CORNER);
-ellipse(250, 250, 100, 100);
+function draw() {
 
-
-rectMode(CENTER);
-rect(250, 250, 100, 100);
 }
