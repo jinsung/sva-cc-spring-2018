b03e425e62765525a46850d545a8d5704afa6622
yumitogo <yumiyumi0123@gmail.com>
2018-01-19 21:12:03 -0500

    Color Test

--- work/yumi/week_02/01_Shape/sketch.js
+++ work/yumi/week_02/01_Shape/sketch.js
@@ -1,17 +1,12 @@
 function setup(){
   createCanvas(500, 500);
-  background(230);
-
-  noStroke();
-  fill(255, 0, 0);
-  ellipse(250, 250, 100, 100);
-
-  fill(0, 255, 0);
-  rect(250, 250, 100, 100);
+  background(0);
 
 }
 
 function draw(){
-
+  noStroke();
+  fill(0, 255 - mouseX, 255 - mouseY);
+  ellipse(250, 250, mouseX, mouseY);
 
 }
