f0b07424b5ed20fa3d4fb9c8fdeba67f963ed920
Youngjin <ylim7@sva.com>
2018-01-19 21:02:20 -0500

    Revert "shape..."
    This reverts commit 8ce120fb6e9d895206093e4d4bf42f45e6d1d610.

--- work/youngjin/week02/01_shape/sketch.js
+++ work/youngjin/week02/01_shape/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
 createCanvas(500, 500);
-}
+background(200);
 
-function draw() {
+fill(255, 0, 0);
+ellipseMode(CORNER);
+ellipse(250, 250, 100, 100);
 
+
+rectMode(CENTER);
+rect(250, 250, 100, 100);
 }
