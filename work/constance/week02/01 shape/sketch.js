c62ef62fe8c6b62e6e89279c742a90a6b0e5bdbd
constanceread <constanceread@live.co.uk>
2018-01-26 14:05:29 -0500

    2 circles moving

--- work/constance/week02/01 shape/sketch.js
+++ work/constance/week02/01 shape/sketch.js
@@ -1,13 +1,39 @@
-function setup(){
-  createCanvas(500,500);
-background(200,30,150);
+var circleX=0;
+var circleY=700;
+var circleA=600;
 
+
+function setup() {
+  createCanvas(1000,700);
+
+  background(220,40,210);
 }
 
-function draw(){
 
-noStroke();
-fill(mouseX*0.5);
-ellipse(250,250,mouseX,mouseY);
+
+function draw() {
+
+ //circle
+   fill(random(250),random(200),200);
+   ellipse(circleX,circleX,circleX,circleX);
+
+   circleX=circleX+5
+
+//secondcircle
+   fill(random(250),random(20),random(200),100);
+   ellipse(200,circleY,circleY,circleY);
+
+   circleY=circleY-6
+
+   //third circle
+
+   //fill(250,random(200),random(200));
+   //ellipse(circleA,circleX,circleY,circleX);
+
+   circleA=circleA+6
+
+   //square
+   //rect(400,400,circleA,circleA);
+
 
 }
