649b420ba49594afc08cbb471e9064194de4f9a5
kuangye123 <kye@sva.edu>
2018-01-26 21:28:11 -0500

    week03

---
+++ work/kuangye/week3/01_variable/sketch.js
@@ -0,0 +1,32 @@
+var circleY;
+var circleX;
+
+var circleXSpeed;
+var circleYSpeed;
+
+function setup(){
+  createCanvas(500, 600);
+  circleY = 250;
+  circleX = 250;
+  circleXSpeed = 5;
+  circleYSpeed = 10;
+}
+
+function draw() {
+  background(200);
+  ellipse(circleX, circleY, 20, 20 );
+
+  circleY = circleY + circleYSpeed;
+  circleX = circleX + circleXSpeed;
+
+  if (circleX < 0 || circleX > 500) {
+    circleXSpeed = circleXSpeed * -1;
+  }
+
+  if (circleY < 0 || circleY > 500) {
+    circleYSpeed = circleYSpeed * -1;
+  }
+
+  }
+  
+  // console.log("circleX is " + circleX);
