cfce589716d8e93a9e2c7f9f4518493d4fdaa795
Youn Jae Lee <younjae.design@gmail.com>
2018-01-26 21:25:03 -0500

    week 3 work

---
+++ work/younjae/week 03/01_variable/sketch.js
@@ -0,0 +1,30 @@
+var circleY;
+var circleX;
+
+var circleXSpeed;
+var circleYSpeed;
+
+function setup() {
+  createCanvas(500, 500);
+  circleY = 250;
+  circleX = 250;
+  circleXSpeed = 5;
+  circleYSpeed = 10;
+}
+function draw() {
+  background(200);
+  ellipse(circleX, circleY, 80, 80);
+
+  circleY = circleY + circleYSpeed;
+  circleX = circleX + circleXSpeed;
+
+  if (circleX < 0 || circleX > width ) {
+    circleXSpeed = circleXSpeed * -1;
+  }
+
+
+  if (circleY < 0 || circleY > height ) {
+   circleYSpeed = circleYSpeed * -1;
+ }
+
+}
---
+++ work/younjae/week 03/02_random/sketch.js
@@ -0,0 +1,21 @@
+var myRandomNumX;
+var myRandomNumY;
+
+var myRandomNumR
+
+function setup() {
+  createCanvas(500, 500);
+  background(200);
+}
+
+function draw() {
+ myRandomNumX = random(width);
+ myRandomNumY = random(height);
+ myRandomNumR = random(255);
+
+ noStroke();
+ stroke(2, 0, 0, 100)
+ fill(myRandomNumR, 0, 100, 100);
+ ellipse(myRandomNumX, myRandomNumY, 50, 50);
+
+}
