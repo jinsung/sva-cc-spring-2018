ed3e650d01e19e2e2e002339cd5f743e8fc0b782
Hyomin <hkim160@sva.edu>
2018-02-02 21:30:50 -0500

    week4

---
+++ work/Hyomin/week04/04_nested_loop/sketch.js
@@ -0,0 +1,19 @@
+function setup () {
+  createCanvas(500, 500);
+}
+
+function draw(){
+  background(200);
+
+  fill(255);
+  var space=20
+
+
+  for ( var circleX = 0; circleX < width; circleX = circleX + space){
+    for ( var circleY = 0; circleY < height; circleY = circleY + 50){
+    ellipse(circleX,circleY,10,10);
+  }
+ }
+
+
+}
