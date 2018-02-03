37369d0f2ee1bd0a7187bd94aae03a5fc2ae9f94
Sean Kirkpatrick <mbp@SKMacBook-2.local>
2018-02-02 21:30:54 -0500

    week 4

---
+++ work/sean/week04/02_while_loop/sketch.js
@@ -0,0 +1,20 @@
+var circleX
+
+function setup() {
+  createCanvas(500,500);
+  var circleX = 1;
+}
+
+function draw () {
+  background(200);
+
+  fill (255);
+
+  circleX = 0;
+
+  while (circleX < width){
+    circleX = circleX + 20;
+    ellipse (circleX, height/2, 10, 10);
+  }
+
+}
---
+++ work/sean/week04/05_random/sketch.js
@@ -0,0 +1,35 @@
+var time = 0;
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw () {
+  background(200);
+
+  fill (255);
+  var space = 20;
+  var circleSize = 10;
+  time = time + 0.02;
+
+  for ( var circleX = 0; circleX < width; circleX = circleX + space ){
+    for ( var circleY = 0; circleY < height; circleY = circleY + space ){
+      //circleSize = random(5, 10);
+      //circleSize = (Math.sin(circleX * 0.26) + Math.cos(circleY * 0.5)) * 4 + 3;
+      var noiseVal = noise( circleX * 0.009, circleY * 0.00451, time );
+      circleSize= noiseVal * 13.0;
+      fill (mouseX, mouseY, 150);
+      ellipse(circleX, circleY, circleSize, circleSize);
+
+    }
+  }
+
+  /*
+  for ( var circleX = 0; circleX < width; circleX = circleX + space ){
+    ellipse (circleX, 0, 10, 10);
+  }
+
+  for ( var circleY = 0; circleY < height; circleY = circleY + space ){
+    ellipse (0, circleY, 10, 10);
+  }
+  */
+}
