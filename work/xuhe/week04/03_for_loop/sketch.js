d74c706f6b5b5f51a1d6d3627e1196e4a5c450ca
xhe2 <hexu@hexudeMacBook-Pro.local>
2018-02-16 15:55:13 -0500

    assignment_practice 1

--- work/xuhe/week04/03_for_loop/sketch.js
+++ work/xuhe/week04/03_for_loop/sketch.js
@@ -1,19 +1,18 @@
-var circleX=0;
+
 function setup() {
   createCanvas(500,500);
-
+  circle=0;
 }
 
 function draw() {
   background(0);
 
   fill(255);
-  circleX=0;
 
-  while(circleX< width){
-  circleX=circleX+20;
+  for(var circleX;circleX=0;circleX<width;circleX=circleX+20){
   ellipse(circleX,height/2,10,10);
- }
+  }
+
 
 
 }
