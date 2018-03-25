4b83a852eb91bb32287ce56c8172b2d3fcfa067e
Sean Kirkpatrick <mbp@SKMacBook.nyc.rr.com>
2018-03-25 01:14:07 -0400

    week 10

--- week10/examples/01_array_and_for_loop_bouningBalls/sketch.js
+++ week10/examples/01_array_and_for_loop_bouningBalls/sketch.js
@@ -2,7 +2,7 @@ var balls = [];
 function setup() {
   createCanvas(500, 500);
   smooth();
-  var numOfBalls = 5;
+  var numOfBalls = 200;
 
   for (var i=0; i<numOfBalls; i++) {
     balls[i] = new BouncingBall();
