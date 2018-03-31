920104a06ae95a54cfe9e9e77cf3b3d79491f16d
Youngjin <ylim7@sva.edu>
2018-03-30 21:30:37 -0400

    week 11

---
+++ work/youngjin/week11/03_mover2/sketch.js
@@ -0,0 +1,22 @@
+var mover;
+function setup() {
+  createCanvas(500, 500);
+  mover = new Mover();
+}
+
+function draw() {
+  background(27, 161, 226);
+  mover.update();
+  mover.display();
+  if (mouseIsPressed) {
+    addMouseForce();
+  }
+}
+
+function addMouseForce() {
+  var mouseLoc = createVector(mouseX, mouseY);
+  var moverLoc = mover.loc;
+  var diffLoc = mouseLoc.sub(moverLoc);
+  diffLoc.mult(0.003);
+  mover.applyForce(diffLoc);
+}
