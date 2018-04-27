7e4e958bd0407caab83e9a82c6c5615ce280d587
jisoo966 <32002313+jisoo966@users.noreply.github.com>
2018-04-27 17:12:16 -0400

    week 14

---
+++ work/younjae/Assignment/Project_3/01_firework/particle.js
@@ -0,0 +1,37 @@
+class Particle {
+  constructor(loc, size) {
+    this.loc = loc.copy();
+    this.size = size;
+    this.acc = createVector(0, 0);
+    this.vel = createVector(0, 0);
+    this.isDead = false;
+    this.age = 0;
+    this.lifeSpan = random (60, 110);
+    this.size = 10;
+
+
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+    this.vel.mult(0.98);
+    this.acc.mult(0);
+    this.age++;
+
+    if (this.age > this.lifeSpan) {
+      this.isDead = true;
+    }
+  }
+
+  display() {
+    var life = (this.lifeSpan - this.age) / this.lifeSpan;
+    fill(HSB * life);
+    var size = this.size * life;
+    ellipse(this.loc.x, this.loc.y, size);
+  }
+}
---
+++ work/younjae/Assignment/Project_3/02/sketch.js
@@ -0,0 +1,30 @@
+var building;
+
+function setup() {
+  createCanvas(1980, 1080);
+  colorMode(HSB);
+
+}
+
+
+function draw() {
+  background(30);
+
+  fill(255);
+  fill(150,150,150,150);
+  noStroke();
+  rect(720,300,20,1500);
+  rect(680,450,100,1500);
+  rect(630,500,200,1500);
+  rect(580,600,300,1500);
+
+
+
+
+
+
+
+
+
+
+}
