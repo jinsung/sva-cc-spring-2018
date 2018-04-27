626971babca0843dffb8d7ddd92fdae0b8c5e529
jisoo966 <32002313+jisoo966@users.noreply.github.com>
2018-04-27 17:20:25 -0400

    s

--- work/younjae/Assignment/Project_3/02/sketch.js
+++ work/younjae/Assignment/Project_3/02/sketch.js
@@ -1,28 +1,18 @@
-var building;
+var bird;
 
 function setup() {
-  createCanvas(1980, 1080);
-  colorMode(HSB);
+  createCanvas(1000, 1000);
+  bird = loadImage("bird.jpg");
+  background(0);
 
 }
 
-
 function draw() {
-  background(30);
-
-  fill(255);
-  fill(150,150,150,150);
-  noStroke();
-  rect(720,300,20,1500);
-  rect(680,450,100,1500);
-  rect(630,500,200,1500);
-  rect(580,600,300,1500);
-
-
-
-
-
-
+  x = random(width);
+  y = random(height);
+  color = bird.get(int(x), int(y));
+  fill(color);
+  ellipse(x, y, 16, 16);
 
 
 
