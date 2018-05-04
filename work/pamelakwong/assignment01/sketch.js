81a423f1a3fd4a36e01426d4e7a88a9deb5db55f
Pamela Kwong <pkwong1@sva.edu>
2018-05-04 15:44:31 -0400

    assignment 01

--- work/pamelakwong/assignment01/sketch.js
+++ work/pamelakwong/assignment01/sketch.js
@@ -1,21 +1,49 @@
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
   background(200);
 }
 
 function draw() {
-  noStroke();
-  fill(mouseX * 0.7);
 
-  var space = 20;
+  var color = 255;
 
-  for (var circleX = 0; circleX < width; circleX = circleX + space) {
-    for (var circleY = 0; circleY < height; circleY = circleY + space) {
-
-      ellipse(mouseY, mouseX, 50, 50);
-      ellipse(mouseY, mouseY, 80, 80);
-
+  var circleSize = 10;
+  var circleSpace = 10;
+  for (var circleY = 0; circleY <= height; circleY += circleSpace) {
+    for(var circleX = 0; circleX <= width; circleX += circleSpace) {
+      noStroke();
+      ellipse(circleX, circleY, circleSize, circleSize);
+      fill(0, 0, 255 - circleY);
     }
   }
 
+  for (var circleY = 0; circleY <= 300; circleY += circleSpace) {
+    for(var circleX = 0; circleX <= 100; circleX += circleSpace) {
+      noStroke();
+      ellipse(circleX, circleY, circleSize, circleSize);
+      fill(0, 0, 255 - circleX);
+    }
+  }
+
+    for (var circleY = 0; circleY <= 400; circleY += circleSpace) {
+      for(var circleX = 0; circleX <= 100; circleX += circleSpace) {
+        noStroke();
+        ellipse(circleX, circleY, circleSize, circleSize);
+        fill(0, 0, 255 - circleX);
+      }
+    }
+
+
+    noStroke();
+    ellipse(mouseX, mouseY, circleSize, circleSize);
+    fill(0, 0, 255 - circleX);
+
+
+
+    rect(width/2, height/2, 400, height);
+    fill(255, 0, 0);
+    rect(200, 200, 50, 50);
+
+    text('Make the circles blue', 400, 400);
+    textAlign(CENTER);
 }
